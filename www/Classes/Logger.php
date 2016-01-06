6fc3d319f89df14039144d3b9efc2d05b155a9fa
dtg <tagedo74@gmail.com>
2016-01-06 15:33:00 +0300

    +

--- www/Classes/Logger.php
+++ www/Classes/Logger.php
@@ -3,26 +3,22 @@ namespace App\Classes;
 use Psr\Log\AbstractLogger;
 
 class Logger
-extends AbstractLogger
-    implements ILogger
+ extends AbstractLogger
+ implements ILogger {
+	public function logToJson($level, $message, $context) {
+		$fileName = __DIR__ . '/../logs/' . date('ymdHis', time());
+		Application::createFile($fileName);
+		$obj = new \stdClass();
+		$obj->date = date('d.m.y H:i:s', time());
+		$obj->level = $level;
+		$obj->message = $message;
+		$obj->context = $context;
+		$json = json_encode($obj);
+		file_put_contents($fileName, $json);
+	}
 
-{
-    public function logToJson($level, $message, $context)
-    {
-        $fileName = __DIR__ . '/../../logs/'.date('ymdHis', time());
-        Application::createFile($fileName);
-        $obj = new \stdClass();
-        $obj->date = date('d.m.y H:i:s', time());
-        $obj->level = $level;
-        $obj->message = $message;
-        $obj->context = $context;
-        $json = json_encode($obj);
-        file_put_contents($fileName, $json);
-    }
-
-    public function log($level, $message, array $context = array()) {
-        self::logToJson($level, $message, $context);
-    }
-
+	public function log($level, $message, array $context = array()) {
+		self::logToJson($level, $message, $context);
+	}
 
 }
